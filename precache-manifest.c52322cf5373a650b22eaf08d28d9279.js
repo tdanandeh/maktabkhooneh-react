9b63930310f2c53eef65f9be751aef576653c0c4
Tohid Danandeh <t.danandeh@gmail.com>
2020-12-01 05:29:56 -0800

    Updates

---
+++ precache-manifest.c52322cf5373a650b22eaf08d28d9279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00734d76e99d4d75b616fec15afe31df",
+    "url": "/maktabkhooneh-react/index.html"
+  },
+  {
+    "revision": "2f019cccdbf59d7716b7",
+    "url": "/maktabkhooneh-react/static/css/2.81ceb5aa.chunk.css"
+  },
+  {
+    "revision": "a6a9ea58264e6b9a4852",
+    "url": "/maktabkhooneh-react/static/css/main.56392cb7.chunk.css"
+  },
+  {
+    "revision": "2f019cccdbf59d7716b7",
+    "url": "/maktabkhooneh-react/static/js/2.653a53db.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/maktabkhooneh-react/static/js/2.653a53db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a9ea58264e6b9a4852",
+    "url": "/maktabkhooneh-react/static/js/main.e0ef8ef7.chunk.js"
+  },
+  {
+    "revision": "cc615474c04b4834b4b4",
+    "url": "/maktabkhooneh-react/static/js/runtime-main.c244dcff.js"
+  }
+]);
