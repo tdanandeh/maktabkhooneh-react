0dc819a2cdad30fc80ac7a73b34459190122024a
Tohid Danandeh <t.danandeh@gmail.com>
2020-12-01 07:42:52 -0800

    first commit

---
+++ src/sections/Navigation.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import "./Navigation.css";
+import { Link } from "react-scroll";
+
+class Navigation extends Component {
+  state = {
+    checked: localStorage.getItem("theme") === "dark" ? true : false,
+    theme: localStorage.getItem("theme"),
+  };
+
+  componentDidMount() {
+    document
+      .getElementsByClassName("top")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+    document
+      .getElementsByClassName("about")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+    document
+      .getElementsByClassName("skills")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+  }
+
+  toggleThemeChange = () => {
+    const { checked } = this.state;
+    if (checked === false) {
+      localStorage.setItem("theme", "dark");
+
+      document
+        .getElementsByClassName("top")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      document
+        .getElementsByClassName("about")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      document
+        .getElementsByClassName("skills")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: true,
+      });
+    } else {
+      localStorage.setItem("theme", "light");
+
+      document
+        .getElementsByClassName("top")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      document
+        .getElementsByClassName("about")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      document
+        .getElementsByClassName("skills")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: false,
+      });
+    }
+  };
+  render() {
+    return [
+      <div className="navigation fixed-top">
+        <nav class="navbar navbar-dark text-light t_s">
+          <div class=" navbarNavAltMarkup">
+            <div class="tohid" >
+              <Link
+                className="test6"
+                to="about"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                <button class="nav-item nav-link text-light bg-transparent border-0 d-inline-block">
+                  درباره ما
+                </button>
+              </Link>
+
+              <Link
+                className="test6"
+                to="skills"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                <button class="nav-item nav-link text-light bg-transparent border-0 d-inline-block">
+                  مهارت ها
+                </button>
+              </Link>
+            </div>
+            <header className="App-header d-inline-block">
+              <label className="switch">
+                <input
+                    type="checkbox"
+                    defaultChecked={this.state.checked}
+                    onChange={() => this.toggleThemeChange()}
+                />
+                <span className="se" >
+                   <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-brush" fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                        d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.117 8.117 0 0 1-3.078.132 3.658 3.658 0 0 1-.563-.135 1.382 1.382 0 0 1-.465-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.393-.197.625-.453.867-.826.094-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.2-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.175-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.247-.013-.574.05-.88.479a11.01 11.01 0 0 0-.5.777l-.104.177c-.107.181-.213.362-.32.528-.206.317-.438.61-.76.861a7.127 7.127 0 0 0 2.657-.12c.559-.139.843-.569.993-1.06a3.121 3.121 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.591 1.927-5.566 4.66-7.302 6.792-.442.543-.796 1.243-1.042 1.826a11.507 11.507 0 0 0-.276.721l.575.575zm-4.973 3.04l.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043l.002.001h-.002z"/>
+                </svg>
+                </span>
+
+              </label>
+            </header>
+          </div>
+        </nav>
+      </div>,
+    ];
+  }
+}
+
+export default Navigation;
